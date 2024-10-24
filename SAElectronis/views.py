3a0dc806d41032d25a653d596f6a9ab2909b8b3c
Scarlett <scarlettd.rojasm@hotmail.com>
2024-10-24 21:32:09 +1000

    fixing errors

--- SAElectronis/views.py
+++ SAElectronis/views.py
@@ -5,3 +5,12 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/item/')
+def item():
+    return render_template('item.html')
+
+@bp.route('/details/')
+def details():
+    return render_template('details.html')
