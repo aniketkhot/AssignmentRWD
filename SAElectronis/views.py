707247dfe2ae7adc36e1efd7a4baf52ff56b5f7a
Scarlett <scarlettd.rojasm@hotmail.com>
2024-10-27 15:44:10 +1000

    half day

--- SAElectronis/views.py
+++ SAElectronis/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, url_for
 
 bp = Blueprint('main', __name__)
+
+
 
 @bp.route('/')
 def index():
@@ -15,14 +17,26 @@ def item():
 def details():
     return render_template('details.html')
 
-@bp.route('/basket/')
+@bp.route('/basket/', methods= ['POST', 'GET'])
 def basket():
     return render_template('basket.html')
 
+# @bp.route('/deletebasket/')
+# def deletebasket():
+#     if 'basket_id' in session:
+#         del session ['order_id']
+#     return render_template('index.html')
+
+# @bp.route('/deletebasketproduct/', methods = ['post'])
+# def deletebasketproduct():
+#     print ('user wants to delete tour with id={}'.format(request.form['id']))
+#     return render_template('index.html')
+
+
 @bp.route('/contactUs/', methods=['POST', 'GET'])
 def contactUs():
-    print('Email: {}\nName: {}'\
-          .format(request.values.get('email'), request.values.get('name')))
+    print('Email: {}\nName: {}\nPhoneNumber: {}\nShippingAdress: {}'\
+          .format(request.values.get('email'), request.values.get('name'), request.values.get('phoneNumber'), request.values.get('shippingAdress')))
     return render_template('contactUs.html')
 
 @bp.route('/aboutUs/')
