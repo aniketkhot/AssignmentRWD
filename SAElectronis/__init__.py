78ea56f9f1d7615e83770f68e4e7f19e8c7b02a0
Aniket <akkhot007@gmail.com>
2024-10-28 16:16:41 +1000

    setting alchemy

--- SAElectronis/__init__.py
+++ SAElectronis/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy 
 
+db = SQLAlchemy()
 
 app=Flask(__name__)
 def create_app():
@@ -7,8 +9,10 @@ def create_app():
     app.debug=True
 
     app.secret_key = 'AniketIsRude'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///saelectronics.sqlite'
     
     from . import views
     app.register_blueprint(views.bp)
-
+    db.init_app(app)
     return app
